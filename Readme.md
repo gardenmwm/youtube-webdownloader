a90bc53abe63ba8f85dd23d820f8c0a6425ded04
Matt Marshall <matt@mattwmarshall.com>
2022-09-17 18:19:29 -0700

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# youtube Web Downloader
+
+This is a tool that supports provides a mechanism for downloading youtube videos with yt-dlp.
+It has a bookmarklet available or you can enter the video manually
+It also works with apple shortcuts
---
+++ ytdownloader.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+import subprocess
+import queue
+import threading
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import time
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ytdownloader.db'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Video(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.String(100), nullable=False)
+    destination = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    channel = db.Column(db.String(100), nullable=False)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/addtoqueue', methods=['POST'])
+def addtoqueue():
+    url = request.form['url']
+    destination = request.form['destination']
+    status = 'Pending'
+    title = 'Unknown'
+    channel = 'Unknown'
+    video = Video(url=url, status=status, destination=destination, title=title, channel=channel)
+    db.session.add(video)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/addtoqueueredirect', methods=['POST'])
+def addtoqueueredirect():
+    url = request.form['url']
+    destination = request.form['destination']
+    status = 'Pending'
+    title = 'Unknown'
+    channel = 'Unknown'
+    video = Video(url=url, status=status, destination=destination, title=title, channel=title)
+    db.session.add(video)
+    db.session.commit()
+    return redirect(url)
+
+
+
+def download_thread(name):
+    while True:
+        video = Video.query.filter_by(status='Pending').first()
+        if video is not None:
+            print("Downloading video")
+            video.status = 'Downloading'
+            db.session.commit()
+            output=subprocess.check_output(['yt-dlp', '-o', os.path.join(video.destination, '%(channel)s - %(title)s.%(ext)s'), '--remux-video', 'mp4', video.url])
+            print(output)
+            video.status = 'Downloaded'
+            db.session.commit()
+        else:
+            time.sleep(10)
+
+
+x=threading.Thread(target=download_thread, args=(1,))
+x.start()
